cdb364eb1e6ac6d2854ade6b6c2c14cb03ea1c14
Mathias Olesen <math09s0@stud.kea.dk>
2023-09-24 23:06:04 +0200

    forgot

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+const parallaxContainer = document.querySelector('.parallax-container');
+const frontWave = document.querySelector('.front-wave');
+const backWave = document.querySelector('.back-wave');
+
+parallaxContainer.addEventListener('mousemove', (e) => {
+    const xAxis = (window.innerWidth / 2 - e.pageX) / 20;
+
+
+    const backWaveXAxis = xAxis * 2.5;
+
+    frontWave.style.transform = `translateX(${xAxis}px)`;
+    backWave.style.transform = `translateX(${backWaveXAxis}px)`;
+});
+
+parallaxContainer.addEventListener('mouseleave', () => {
+    frontWave.style.transform = 'translateX(0)';
+    backWave.style.transform = 'translateX(0)';
+});
+
