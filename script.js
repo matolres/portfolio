27ebdaafa30219257e96cf75defb9c204ac422a8
Mathias Olesen <math09s0@stud.kea.dk>
2023-11-24 12:43:39 +0100

    changes

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
+const parallaxContainer = document.querySelector(".parallax-container");
+const frontWave = document.querySelector(".front-wave");
+const backWave = document.querySelector(".back-wave");
 
+parallaxContainer.addEventListener("mousemove", (e) => {
+  const xAxis = (window.innerWidth / 2 - e.pageX) / 20;
 
-const parallaxContainer = document.querySelector('.parallax-container');
-const frontWave = document.querySelector('.front-wave');
-const backWave = document.querySelector('.back-wave');
+  const backWaveXAxis = xAxis * 3;
 
-parallaxContainer.addEventListener('mousemove', (e) => {
-    const xAxis = (window.innerWidth / 2 - e.pageX) / 20;
-
-
-    const backWaveXAxis = xAxis * 2.5;
-
-    frontWave.style.transform = `translateX(${xAxis}px)`;
-    backWave.style.transform = `translateX(${backWaveXAxis}px)`;
+  frontWave.style.transform = `translateX(${xAxis}px)`;
+  backWave.style.transform = `translateX(${backWaveXAxis}px)`;
 });
 
-parallaxContainer.addEventListener('mouseleave', () => {
-    frontWave.style.transform = 'translateX(0)';
-    backWave.style.transform = 'translateX(0)';
+parallaxContainer.addEventListener("mouseleave", () => {
+  frontWave.style.transform = "translateX(0)";
+  backWave.style.transform = "translateX(0)";
 });
-
